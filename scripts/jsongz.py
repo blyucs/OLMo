c4d6a2ba7e45ac35fea6bf8ffd65a085b29d833c
blyucs <53803532+blyucs@users.noreply.github.com>
2024-12-01 10:29:27 +0800

    prepare update

--- scripts/jsongz.py
+++ scripts/jsongz.py
@@ -27,8 +27,70 @@ import json
 import os
 import logging
 
+url_keywords = {
+    'en.wikipedia.org': 'Wikipedia',
+    '.wikisource.org': 'Wikisource',
+    '.cnn.com': 'CNN',
+    # '.mdpi.com': 'MDPI',
+    # '.nytimes.com': 'NYTimes',
+    # '.archive.org': 'Archive',
+    # 'journals.plos.org': 'PLOS',
+    # 'serverfault.com': 'ServerFault',
+    # 'archive.org': 'ArchiveOrg',
+    # 'spectrum.ieee.org': 'IEEE',
+    # 'nature.com': 'Nature',
+    # 'techrepublic.com': 'TechRepublic',
+    # 'wired.com': 'Wired',
+    # 'physics.stackexchange.com': 'PhysicsStackExchange',
+    # 'zdnet.com': 'ZDNet',
+    # 'superuser.com': 'SuperUser',
+    # 'studymoose.com': 'StudyMoose',
+    # 'softmath.com': 'SoftMath',   # filter1  0 ~ 10000 OK    --- appended by 0~10000  below filters -- killed for full
+##########################################
+#### new  ----
+    # 'mathoverflow.net': 'MathOverflow',
+    # 'dailytech.com': 'DailyTech',
+    # 'britannica.com': 'Britannica',
+    # 'encyclopedia.com': 'Encyclopedia',
+    # 'theregister.co.uk': 'TheRegister',
+    # 'link.springer.com': 'Springer',
+    # 'science.slashdot.org': 'ScienceSlashdot',
+    # 'newworldencyclopedia.org': 'NewWorldEncyclopedia',
+    # 'bookrags.com': 'BookRags',
+    # 'boingboing.net': 'BoingBoing',
+    # 'issuu.com': 'Issuu',
+    # 'phys.org': 'PhysOrg',
+    # 'healthunlocked.com': 'HealthUnlocked',
+    # 'motls.blogspot.com': 'MotlsBlog',
+    # 'genomebiology.biomedcentral.com': 'GenomeBiology',
+# new  0----
+#     'theguardian.com': 'TheGuardian',
+#     'edition.cnn.com': 'EDITION_CNN',
+#     'ufdc.ufl.edu': 'UFDC',
+#     'enotes.com': 'ENotes',
+##############################################
+# new physics   --- all not filtered
+#     'mathwithbaddrawings.com': 'Mathwithbaddrawings',
+#     'nrich.maths.org': 'NrichMath',
+#     'plus.maths.org': 'PlusMath',
+#     'wiki.math.uwaterloo.ca': 'Uwaterloo',
+#     'encyclopediaofmath.org': 'MathEncyclopedia',
+#     'goodmath.org': 'GoodMath',
+#     'hackmath.net': 'HackMath',
+#     'math.columbia.edu': 'MathColum',
+#     'mathworks.com': 'MathSoftware',
+#     'purplemath.com': 'PurpleMath',
+#     'physicsworld.com': 'PhysicsWorld',
+#     'physicsforums.com': 'PhysicsForums',
+#     'physicsbuzz.physicscentral.com': 'PhysicsBuzz',
+#     'physicsoverflow.org': 'PhysicsOverflow',
+#     'math.stackexchange.com': 'MathStackExchange',
+#     'chemistry.stackexchange.com': 'ChemistryStackExchange',
+#     'chemistryworld.com': 'ChemistryWorld',
+}
+
 def setup_logging():
-    logging.basicConfig(filename='jsonl_packing_errors.log', level=logging.ERROR,
+    logging.basicConfig(filename='jsonl_packing_errors.log', level=logging.INFO,
                         format='%(asctime)s:%(levelname)s:%(message)s')
 
 def pack_jsonl_files_to_gz(directory, output_dir, max_size_gb=10):
@@ -36,17 +98,28 @@ def pack_jsonl_files_to_gz(directory, output_dir, max_size_gb=10):
     max_bytes = max_size_gb * 1024 * 1024 * 1024  # Convert GB to bytes
     current_size = 0
     part_num = 0
+    total_size = 0
 
     # Extract the specific directory string for filename
     directory_name = os.path.basename(os.path.normpath(directory))
-    
+
+    # Ensure the directory exists
+    output_path = os.path.join(output_dir, directory_name)
+    os.makedirs(output_path, exist_ok=True)
+
     # Open the first output file
-    gzfile = gzip.open(os.path.join(output_dir, f'{directory_name}/{directory_name}_part_{part_num}.json.gz'), 'wt', encoding='utf-8')
-    
-    for root, dirs, files in os.walk(directory):
-        for filename in files:
+    filename = os.path.join(output_path, f'{directory_name}_part_{part_num}.json.gz')
+    gzfile = gzip.open(filename, 'wt', encoding='utf-8')
+
+    allowed_directories = set(url_keywords.values())
+    # Directly list subdirectories of the main directory
+    subdirectories = [d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d)) and d in allowed_directories]
+
+    for dir_name in subdirectories:
+        current_dir_path = os.path.join(directory, dir_name)
+        for filename in os.listdir(current_dir_path):
             if filename.endswith('.jsonl'):
-                file_path = os.path.join(root, filename)
+                file_path = os.path.join(current_dir_path, filename)
                 with open(file_path, 'r', encoding='utf-8') as file:
                     for line_number, line in enumerate(file, 1):
                         stripped_line = line.strip()
@@ -54,25 +127,30 @@ def pack_jsonl_files_to_gz(directory, output_dir, max_size_gb=10):
                             continue  # Skip empty lines
                         try:
                             json_object = json.loads(stripped_line)
-                            if 'text' in json_object:
+                            if 'text' in json_object: # and json_object['fasttext_openhermes_reddit_eli5_vs_rw_v2_bigram_200k_train_prob'] > 0.5:
                                 text_content = json_object['text']
                                 minimal_json = json.dumps({"text": text_content})
                                 if current_size + len(minimal_json.encode('utf-8')) + 1 > max_bytes:
                                     gzfile.close()
                                     part_num += 1
-                                    gzfile = gzip.open(os.path.join(output_dir, f'{directory_name}/{directory_name}_part_{part_num}.json.gz'), 'wt', encoding='utf-8')
+                                    gzfile = gzip.open(os.path.join(output_path, f'{directory_name}_part_{part_num}.json.gz'), 'wt', encoding='utf-8')
                                     current_size = 0  # Reset the size counter
                                 gzfile.write(minimal_json + '\n')
                                 current_size += len(minimal_json.encode('utf-8')) + 1
+                                total_size += len(minimal_json.encode('utf-8')) + 1
                             else:
-                                logging.error(f"Missing 'text' field at {file_path}, line {line_number}")
+                                print(f"Missing 'text' field at {file_path}, line {line_number}")
                         except json.JSONDecodeError as e:
-                            logging.error(f"Error decoding JSON at {file_path}, line {line_number}: {e}")
-
+                            print(f"Error decoding JSON at {file_path}, line {line_number}: {e}")
+    print(f"Total size {total_size}")
     gzfile.close()
 
 # Example usage
-directory_path = '/home/lvbo/DCLM-data/Run_20241125-114153_0_to_2000/'  # 设置你的文件夹路径
-output_gz_path = '/home/lvbo/01_gz_data/'  # 设置输出文件的路径
-pack_jsonl_files_to_gz(directory_path, output_gz_path, max_size_gb=10)
+# directory_path = '/home/lvbo/DCLM-data/Run_20241125-114153_0_to_2000'  # 设置你的文件夹路径
+# directory_path = '/home/lvbo/DCLM-data/Run_20241125-114925_2000_to_4000'  # 设置你的文件夹路径
+directory_path = '/home/lvbo/DCLM-data/Run_20241125-223437_4000_to_6000'  # 设置你的文件夹路径
+# directory_path = '/home/lvbo/DCLM-data/Run_20241125-114153_0_to_2000'  # 设置你的文件夹路径
+# output_gz_path = '/home/lvbo/02_gz_data/'  # 设置输出文件的路径
+output_gz_path = '/home/lvbo/04_gz_all_common/'  # 设置输出文件的路径
+pack_jsonl_files_to_gz(directory_path, output_gz_path, max_size_gb=20)
 
